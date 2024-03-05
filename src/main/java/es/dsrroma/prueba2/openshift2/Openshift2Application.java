cce98cab3563e11d08750c52a9300fdc9ba773d3
emontalban <emontalban@innovative-systems.es>
2024-03-05 12:13:13 +0100

    first commit

---
+++ src/main/java/es/dsrroma/prueba2/openshift2/Openshift2Application.java
@@ -0,0 +1,13 @@
+package es.dsrroma.prueba2.openshift2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Openshift2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Openshift2Application.class, args);
+	}
+
+}
